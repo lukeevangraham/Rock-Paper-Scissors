a00a1f8d7d3fc0bfa8af8e0046e54b705730c6df
Cefalu Wave <luke.evan.graham@gmail.com>
2020-02-02 21:34:32 -0800

    auth result coming in

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -22,7 +22,8 @@ var uiConfig = {
         // User successfully signed in.
         // Return type determines whether we continue the redirect automatically
         // or whether we leave that to developer to handle.
-        return true;
+        console.log(authResult)
+        return false;
       },
       uiShown: function() {
         // The widget is rendered.
@@ -32,16 +33,16 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    // signInSuccessUrl: '<url-to-redirect-to-on-success>',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.GithubAuthProvider.PROVIDER_ID
     ],
     // Terms of service url.
-    tosUrl: '<your-tos-url>',
+    // tosUrl: '<your-tos-url>',
     // Privacy policy url.
-    privacyPolicyUrl: '<your-privacy-policy-url>'
+    // privacyPolicyUrl: '<your-privacy-policy-url>'
   };
 
   // The start method will wait until the DOM is loaded.
